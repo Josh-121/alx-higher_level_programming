e3a020b4ffe7dce0a42f2ec2acfe48a4bd43182e
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-08-27 21:46:03 +0000

    sort list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,49 +1,40 @@
 #include "lists.h"
 /**
- * insert_node  - insert node according to sorted linked list
- * @head: pointer that store the address of the head pointer
+ * insert_node  - insert listint_t according to sorted linked list
+ * @head: next that store the address of the head next
  * @number: value to insert in the linked list
  *
  * Return: return the address of the new memory allocated
  */
-listint_t *insert_node(listint_t **head, int number)
+listint_t *insert_node(listint_t **head, int value)
 {
-	listint_t *dir = *head, *temp = NULL, *trans = *head, *new = NULL;
-	int find = 0;
+	listint_t *ptr = NULL ;
+    listint_t *temp=NULL;
+    ptr = malloc(sizeof(listint_t));
+    ptr -> n =value;
+    ptr-> next=NULL;
+    temp=*head;
+    
+    if (temp->n > value)
+    {
+        ptr->next=*head;
+        *head=ptr;
+    }
 
-	new = malloc(sizeof(new));
-	if (!new)
-		return (NULL);
-	new->n = number;
-	new->next = NULL;
-	if (!*head)
-	{
-		*head = new;
-		return (new);
-	}
-	if ((*head)->n > number)
-	{
-		*head = new;
-		new->next = dir;
-		return (new);
-	}
-	while (trans->next)
-	{
-		if (trans->next->n > number)
-		{
-			find = 1;
-			temp = trans;
-			break;
-		}
-		trans = trans->next;
-	}
-	if (find)
-	{
-		dir = temp->next;
-		temp->next = new;
-		new->next = dir;
-		return (new);
-	}
-	trans->next = new;
-	return (new);
+    else
+    {
+        while (1)
+    {
+        if (temp->next== NULL || temp->next->n > value)
+        {
+            ptr->next=temp->next;
+            temp->next=ptr;
+            break;
+        }
+        
+        temp=temp->next;
+    }
+
+    }
+    return *head;
 }
