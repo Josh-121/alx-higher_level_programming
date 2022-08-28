fae8d2cb5ff337fdcc93ab96ccacfd05cb22d6ab
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-08-28 23:12:22 +0000

    changes

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    newlist = []
-    tuple_a_list = list(tuple_a)
-    tuple_b_list = list(tuple_b)
-    i = 0
-    while i < 2:
-        if len(tuple_a_list) < 2:
-            tuple_a_list.append(0)
-        if len(tuple_b_list) < 2:
-            tuple_b_list.append(0)
-        newlist.append(tuple_a_list[i] + tuple_b_list[i])
-        i += 1
-    return tuple(newlist)
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
