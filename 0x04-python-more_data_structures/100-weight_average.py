1505e0061b09e01fd3be0762b609eea6ebcebe84
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-08-31 20:29:53 +0000

    additions

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     sum = 0
     weightSum = 0
+    if len(my_list) == 0:
+        return 0
     for i in my_list:
         sum += i[0] * i[1]
         weightSum += i[1]
