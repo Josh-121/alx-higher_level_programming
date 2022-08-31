a4ee2e4997256a7010a2267eb7b68967b8145a55
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-08-31 19:31:53 +0000

    more data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,7 @@ def best_score(a_dictionary):
         return None
     for key in a_dictionary:
         max = a_dictionary[key]
+        maxKey = key
         break
     for i in a_dictionary:
         if a_dictionary[i] > max:
