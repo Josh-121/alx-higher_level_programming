d037d42baf706c5e752c6f0ed182cb15f8807021
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-09-01 00:03:02 +0000

    update

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    integer = 0
+    index = 0
+    diffSum = 0
+    valueSum = 0
+    try:
+        length = len(roman_string)
+        while index < length:
+            if index + 1 < length:
+                key = roman_string[index]
+                nextKey = roman_string[index + 1]
+                if dic[key] < dic[nextKey]:
+                    diffSum += dic[nextKey] - dic[key]
+                    index += 2
+                    key = roman_string[index]
+                    nextKey = roman_string[index + 1]
+            if index < length:
+                valueSum += dic[key]
+            index += 1
+        integer = diffSum + valueSum
+        return integer
+    except TypeError:
+        return 0
