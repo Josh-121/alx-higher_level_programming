bab2c7a71c85e8797833089d755fa2f7d9ba8174
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-09-13 15:37:59 +0000

    OOP

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        count = 0
+        while True:
+            if count == self.area():
+                break
+            print('#', end="")
+            count += 1
+            if count % self.__size == 0:
+                print()
+        if count == 0:
+            print()
